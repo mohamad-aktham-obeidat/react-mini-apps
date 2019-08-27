7aa6f197ce2d373fb8d73f549e955994801f1b95
mohamadAkthamObeidat <mohamadaktham7@gmail.com>
2019-08-27 21:41:43 +0300

    Start Working On React Mini Apps

---
+++ mini-apps/src/Componenets/FbCommentCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const FbCommentCard = () => {
+    return (
+        <div>
+            <h1>Exercise #1</h1>
+            <div className="comment">
+                
+            </div>
+        </div>
+    );
+}
+
+export default FbCommentCard;
